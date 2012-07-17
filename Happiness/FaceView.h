72e4a2175c13c670dc7b369abd55350ee0d6f4b5
m2m tech <tech@m2m.at>
2012-07-17 13:41:33 +0200

    status end of lecture #5

---
+++ Happiness/FaceView.h
@@ -0,0 +1,25 @@
+//
+//  FaceView.h
+//  Happiness
+//
+//  Created by Martin Mandl on 17.07.12.
+//  Copyright (c) 2012 m2m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaceView : UIView
+
+#define DEFAULT_SCALE 0.9
+#define EYE_H 0.35
+#define EYE_V 0.35
+#define EYE_RADIUS 0.1
+#define MOUTH_H 0.45
+#define MOUTH_V 0.4
+#define MOUTH_SMILE 0.25
+
+@property (nonatomic) CGFloat scale;
+
+- (void)pinch:(UIPinchGestureRecognizer *)gesture;
+
+@end
